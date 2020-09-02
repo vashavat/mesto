d6f8a4cd5fdebb248d8bba7e2cc24f217e3e2c00
vashavat <vashavat@gmail.com>
2020-09-02 14:33:47 +0300

    feat: добавить открывание и закрывание попапа

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-console.log('h');
+const popup = document.querySelector('.popup');
+const popupEditButton = document.querySelector('.button_type_edit');
+const popupCloseButton = popup.querySelector('.button_type_close');
+
+const popupToggle = function () {
+  popup.classList.toggle('popup_is-opened');
+}
+
+const popupCloseByClickOverlay = function () {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  popupToggle();
+}
+
+popupEditButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+popup.addEventListener('click', popupCloseByClickOverlay);
